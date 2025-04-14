01303c0ef3d5485c759d45c2614387a1a94447f8
Armand Nobre <nobrearmand@gmail.com>
2025-04-14 15:52:43 +0200

    navigation entre pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { Routes, Route } from "react-router-dom";
 
 // Use lazy loading to optimize bundle size
 const HomePage = lazy(() => import("./pages/HomePage"));
+const Login = lazy(() => import("./pages/auth/Login"));
+const Register = lazy(() => import("./pages/auth/Register"));
 
 function App() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Suspense>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </li>
+            </ul>
             <App />
         </BrowserRouter>
     </React.StrictMode>
