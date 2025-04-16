0f97cce5a375137e73e703714dd4145fabea1cad
Tom.T <tomtamen@gmx.fr>
2025-04-16 10:03:30 +0200

    feat: authPostConfirmation

--- amplify/backend/function/authPostConfirmation/src/custom.js
+++ amplify/backend/function/authPostConfirmation/src/custom.js
@@ -1,7 +1,37 @@
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
 /**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
  */
-exports.handler = async (event, context) => {
-  // insert code to be executed by your lambda trigger
+exports.handler = async (event) => {
+  console.log("EVENT: ", JSON.stringify(event));
+
+  const { userAttributes } = event.request;
+
+  // Set up the DynamoDB client
+  const client = new DynamoDB({ region: "eu-west-1" });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+  const user = {
+    id: userAttributes.sub,
+    name: userAttributes.name || '',
+    lastname: userAttributes.family_name || '',
+    birthdate: userAttributes.birthdate || '',
+    email: userAttributes.email || '',
+    phone: userAttributes.phone_number || '',
+    nickname: userAttributes.nickname || '',
+    createdAt: new Date().toISOString()
+  };
+
+  try {
+    await ddbDocClient.send(new PutCommand({
+      TableName: "users",
+      Item: user
+    }));
+  } catch (err) {
+    console.error("Error adding user to DynamoDB:", err);
+  }
+
   return event;
 };
